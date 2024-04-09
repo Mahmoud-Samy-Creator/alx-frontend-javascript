d7b82eeea451af523940dc1c1a7f3ed7542e3072
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-09 02:00:28 +0200

    task [4]

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,9 +1,3 @@
 export default function returnHowManyArguments(...args) {
-  let total = 0;
-  /* eslint-disable no-unused-vars */
-  for (const arg of args) {
-    total += 1;
-  }
-  /* eslint-enable no-unused-vars */
-  return total;
+  return args.length;
 }
