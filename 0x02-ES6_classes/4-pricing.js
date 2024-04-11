0d88ffe0f136a9b45ba9598c9a7b3010e4c657c6
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-11 17:41:41 +0200

    task [4]

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -30,7 +30,7 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this.currency.displayFullCurrency()}`;
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
   }
 
   static convertPrice(amount, conversionRate) {
