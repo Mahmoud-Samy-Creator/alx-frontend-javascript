6137649d4ae1a44d1c56cd49cf314c2db727db8d
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-11 19:20:51 +0200

    task [7]

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,36 @@
+class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string' && !(name instanceof String)) {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string' && !(code instanceof String)) {
+      throw new TypeError('Name must be a string');
+    }
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
+
+const airportSF = new Airport('San Francisco Airport', 'SFO');
+console.log(airportSF);
+console.log(airportSF.toString());
