6d24b9cf0fb4b6c5666a93adb86838a0ddc99f39
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-11 16:35:15 +0200

    task [2]

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -10,7 +10,7 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
-    if ((typeof newName !== 'string') && !(name instanceof String)) {
+    if ((typeof name !== 'string') && !(name instanceof String)) {
       throw new TypeError('Name must be a string');
     }
     this._name = name;
@@ -21,7 +21,7 @@ export default class HolbertonCourse {
   }
 
   set length(length) {
-    if (typeof newLength !== 'number' && !(length instanceof Number)) {
+    if (typeof length !== 'number' && !(length instanceof Number)) {
       throw new TypeError('Length must be a number');
     }
     this._length = length;
