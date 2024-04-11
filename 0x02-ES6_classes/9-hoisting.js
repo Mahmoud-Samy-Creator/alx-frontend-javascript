bc444068e75a7e13cf553ae4e7705e1ed87b0e1e
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-11 19:58:18 +0200

    task [9]

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -11,10 +11,6 @@ export class HolbertonClass {
   get location() {
     return this._location;
   }
-
-  get holbertonClass() {
-    return this.holbertonClass;
-  }
 }
 
 export class StudentHolberton {
@@ -31,6 +27,10 @@ export class StudentHolberton {
   get fullStudentDescription() {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
+
+  get holbertonClass() {
+    return this.holbertonClass;
+  }
 }
 
 const class2019 = new HolbertonClass(2019, 'San Francisco');
@@ -43,3 +43,4 @@ const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
 export const listOfStudents = [student1, student2, student3, student4, student5];
+export default listOfStudents;
