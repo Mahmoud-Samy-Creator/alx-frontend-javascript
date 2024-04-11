c1fd2989c286f53b75ee9674b18621c554f95671
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-11 02:39:32 +0200

    task [2]

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({ staus: 200, body: 'success' }))
+    .then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
