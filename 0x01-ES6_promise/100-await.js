6c3a90ce6483772585bea6e51fd5929b3dfff0c2
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-11 05:25:30 +0200

    Advanced task [0]

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,14 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  let res = {};
+  let result = {};
 
   try {
     const photo = await uploadPhoto();
     const user = await createUser();
-    res = { photo, user };
+    result = { photo, user };
   } catch (err) {
-    res = { photo: null, user: null };
+    result = { photo: null, user: null };
   }
-  return res;
+  return result;
 }
