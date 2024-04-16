ae3a484265cd251d673b214c38d2768ff5d35873
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-16 21:35:27 +0200

    task [8]

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   if (startString) {
-    const arr = [...set].filter((obj) => obj.startsWith(startString))
+    const arr = [...set].filter((obj) => typeof obj === 'string').filter((obj) => obj.startsWith(startString))
       .map((obj) => obj.replace(startString, ''));
     return arr.join('-');
   }
